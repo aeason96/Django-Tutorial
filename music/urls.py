89984f884a4b84e2399499721ae43e26d1206f3c
Andrew <aeason96@vt.edu>
2016-10-05 14:01:54 -0400

    update

--- music/urls.py
+++ music/urls.py
@@ -5,9 +5,12 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     # /music/71/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    #/music/album/add
+    url(r'^album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 ]
 
